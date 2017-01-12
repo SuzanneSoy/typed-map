66b9cb6f434dd2b0d3ff78743f16e6b2ed2d2a25
Georges Dupéron <georges.duperon@gmail.com>
2017-01-12 21:57:39 +0100

    Fix issues with splitting the package

--- typed-map-doc/info.rkt
+++ typed-map-doc/info.rkt
@@ -3,7 +3,9 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib"
                      "racket-doc"
-                     "typed-racket-doc"))
+                     "typed-racket-doc"
+                     "aful"
+                     "typed-map-lib"))
 (define pkg-desc
   (string-append "Documentation for typed-map-lib, a"
                  " type inference helper for map with Typed/Racket."
