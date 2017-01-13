527d624a14fe3c73dd0f25d6f1fb75089df2d7c1
Georges Dupéron <georges.duperon@gmail.com>
2017-01-13 17:03:13 +0100

    Fixed dependency problem on typed-map-doc

--- typed-map/info.rkt
+++ typed-map/info.rkt
@@ -1,9 +1,11 @@
 #lang info
 (define collection 'multi)
 (define deps '("typed-map-lib"
-               "typed-map-test"))
+               "typed-map-test"
+               "typed-map-doc"))
 (define implies '("typed-map-lib"
-                  "typed-map-test"))
+                  "typed-map-test"
+                  "typed-map-doc"))
 (define build-deps '())
 (define pkg-desc
   (string-append "Type inference helper for map with Typed/Racket."
